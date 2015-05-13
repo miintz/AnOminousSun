82745bf3ba2e5a31e90028a43991ccfebf9f8c14
Maarten van Hees <maarten@lingewoud.com>
2015-05-13 16:46:40 +0200

    maarten: reverted, going with NAudio

--- Handler/Handler/Form1.cs
+++ Handler/Handler/Form1.cs
@@ -10,47 +10,28 @@ using System.Windows.Forms;
 using System.Threading;
 using System.IO.Ports;
 
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework;
-using System.IO;
-
-using GdiColor = System.Drawing.Color;
-using XnaColor = Microsoft.Xna.Framework.Color;
-
 namespace Handler
 {
     public partial class Handler : Form
     {
         SerialPort port;
-        MicrophoneMonitor Monitor;
-
+        
         public Handler()
         {
             InitializeComponent();
 
         }
 
-        public void SetMicrophoneMonitorInstance(MicrophoneMonitor m)
-        {
-            Monitor = m;
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            Monitor.StartMic();
-
             port = new SerialPort("COM3", 57600);
 
             try
-            {
-                //un-comment this line to cause the arduino to re-boot when the serial connects
-                //port.DtrEnabled = true;
-
+            {                
                 port.Open();
             }
             catch (Exception ex)
-            {
-                //alert the user that we could not connect to the serial port
+            {             
                 this.Close();
                 this.Dispose();
             }
@@ -63,7 +44,7 @@ namespace Handler
 
         private void FireListener()
         { 
-        
+            //9
         }
 
         private void ResetListener()
--- Handler/Handler/Program.cs
+++ Handler/Handler/Program.cs
@@ -15,27 +15,10 @@ namespace Handler
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        {            
             Application.EnableVisualStyles();
-         
-            Handler f = new Handler();
-
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Handler());
-
-            f.ClientSize = new System.Drawing.Size(300, 60);
-            MicrophoneMonitor game = new MicrophoneMonitor();
-            f.SetMicrophoneMonitorInstance(game);
-            ((Action)(() => System.Windows.Forms.Application.Run(f))).BeginInvoke(null, null);
-            
-            using (game)
-            {
-                f.ResizeEnd += new EventHandler(game.f_LocationChanged);
-                
-                game.Run();
-            }
-
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Handler());
         }
     }
 }
